b7d5fc9a1291ef1379e8ab79075549bd0492db0e
sp1d5r <aa1719@imperial.ac.uk>
2021-08-23 18:35:13 +0100

    Backend - Added Hand Tracking MModel and output.

--- HandTracker.py
+++ HandTracker.py
@@ -3,10 +3,26 @@ import mediapipe as mp
 import time
 
 if __name__=="__main__":
-    capture = cv2.VideoCapture(1)
+    capture = cv2.VideoCapture(0)
+
+    # We can use the Hand Detection Model from media pipe
+    mp_hands = mp.solutions.hands
+    hands = mp_hands.Hands()
+    mpDraw = mp.solutions.drawing_utils
 
     while True:
         success, img = capture.read()
+
+        # Data Preprocessing - Convert the image to RGB
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        # Applying the model to the image
+        results = hands.process(img_rgb)
+
+        # Extract out the information for the hands
+        if results.multi_hand_landmarks:
+            for hand in results.multi_hand_landmarks:
+                mpDraw.draw_landmarks(img, hand, mp_hands.HAND_CONNECTIONS)
 
         cv2.imshow("Image", img)
         cv2.waitKey(1)
---
+++ README.md
@@ -0,0 +1,10 @@
+# Handshakes 
+In the most awkward interaction I had yesterday I realised I'm horrible at handshakes. 
+For some reason i can't remember the sophisticated movements my hands are supposed to do.
+Left? Right? ...? Why is it so complicated. Damn this shit is hard. 
+
+Okay so the plan is to make software that tracks hands, so i can practice my handshakes by 
+myself... Wow, that is the saddest sentence i have ever written. It's okay I'll think of a 
+better way of writing it later.
+
+## 
